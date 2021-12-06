d7487db3a860244c95fb527df28fa4154da60694
Tarun Punhani <tarun@Taruns-MacBook-Pro.local>
2021-12-05 23:33:52 -0600

    Update

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -4,7 +4,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
-{
+{   
+    private AudioSource audio;
+
+    private void Awake(){
+        DontDestroyOnLoad(transform.gameObject);
+        audio = GetComponent<AudioSource>();
+    }
+
+
+    public void PlayMusic(){
+        if(audio.isPlaying) return;
+        audio.Play();
+    }
+
+
+    public void StopMusic(){
+        audio.Stop();
+    }
+
     public void StartGame(){
         SceneManager.LoadScene("Level1");
     }
